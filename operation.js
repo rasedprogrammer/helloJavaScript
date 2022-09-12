c00fd96a91e71cd7901f5528e38edacc6ffb6f1b
Md Rased Molla <rasedprogrammer@gmail.com>
2022-09-12 16:40:03 +0600

    adding practice problem

---
+++ operation.js
@@ -0,0 +1,22 @@
+﻿var onionPrice = 200;
+var onionQuenty = 5;
+var onion = 10;
+var eggPrice = 30;
+// console.log(onionPrice);
+// console.log(eggPrice);
+
+var totalPrice = onionPrice + eggPrice;
+var priceDiffrence = onionPrice - eggPrice;
+var totalPrice = onionPrice * onionQuenty;
+var totalPrice = onionPrice + eggPrice;
+// console.log(sum);
+
+var price = 30;
+price = 40;
+
+var price1 = 20;
+var price2 = 50;
+
+var price3 = price1 + 10;
+
+price2 += 20;
---
+++ practiceProblem.js
@@ -0,0 +1,34 @@
+﻿// Problem Number 1
+var maDiloTaka = 1000;
+console.log(maDiloTaka);
+var orangePrice = 300;
+var mangoPrice = 400;
+var totalFruitsPrice = orangePrice + mangoPrice;
+console.log(totalFruitsPrice);
+var bakiTakaThaklo = maDiloTaka - totalFruitsPrice;
+console.log(bakiTakaThaklo);
+
+// Problem Number 2
+var bangla = 75.25;
+var english = 65;
+var math = 80;
+var ict = 35.45;
+var bioloogy = 99.5;
+
+var totalMark = bangla + english + math + ict + bioloogy;
+
+var avgMark = totalMark / 5;
+
+var avgOutPut = avgMark.toFixed(2);
+console.log(avgOutPut);
+
+// Problem Number 3
+var firstLine = "I am going to be";
+var lastLine = "an awesome web developer";
+var fullLine = firstLine + " " + lastLine;
+console.log(fullLine);
+
+// Problem Number 3
+var number = 119;
+var numberReminder = number % 5;
+console.log(numberReminder);
