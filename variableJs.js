d4344df462336bd6fc7bdc9dc61fea88883bcee3
Md Rased Molla <rasedprogrammer@gmail.com>
2022-09-12 13:47:52 +0600

    adding variable name

--- variableJs.js
+++ variableJs.js
@@ -1,4 +1,27 @@
 ﻿// console.log(7);
-var price = 30;
-var money = 999;
-console.log(money);
+// var price = 30;
+// var money = 999;
+// console.log(money);
+
+// String Type Variable
+var name = 'Rased Molla';
+
+// Number Type Variable
+var age = 26;
+
+// Boolean Type Variable
+var isWatherRain = true;
+var youJobHolder = true;
+var youMarried = false;
+
+
+
+// // Boolean Type Variable
+// var apple = 30;
+// var grap = 50;
+// if (grap > apple){
+// 	console.log("Grap");
+// }else{
+// 	console.log("Apple");
+// }
+// // Result   Grap
---
+++ variableName.js
@@ -0,0 +1,21 @@
+﻿// 1. Variable name can not be any keyword
+// var true = "Yes it is";
+
+// 2. Variabe name has to be one word
+// var my name is = "Md Rased Molla";
+
+// 3. Variable name can not have quotation.
+// var "myname" = "Rased";
+
+// 4. Variable name can not starts with a start but can ends with number
+// var 99rased = 000;
+
+// 5. Variable How to use long name
+var usehomeaddress = "Badda, Dhaka";
+var use_home_address = "Badda, Dhaka"; //Snake Case
+var useHomeAddress = "Badda, Dhaka"; // Camel Case
+var UseHomeAddress = "Badda, Dhaka"; // Pascal Case
+
+// 6. Variable name is case sensitive
+var price = 25;
+var Price = 35;
